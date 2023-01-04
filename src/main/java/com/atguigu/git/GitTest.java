7bfe96287ad799a7908dbe808a7d07d0641ca866
Tunan0109 <2654843057@qq.com>
2023-01-05 00:53:27 +0800

    master first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author: workershll
+ * @Create: 2023/1/5 - 0:33
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
